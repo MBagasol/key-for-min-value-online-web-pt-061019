88a9d9272861a11a4c1add05b04282e7c514977d
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-20 20:30:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,12 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |adam, blake|
-    
+    if adam = lowest_key
+      lowest_key
+    elsif blake = lowest_value
+      lowest_value
+    else 
+      return lowest_key
+    end
+  end
 end
